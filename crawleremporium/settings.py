af0d81ecc5af8e4103e5b3a4e983cc8b58ef4012
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-06-30 13:16:48 +0100

    Add: Created templates in lootboxes app, created index.html

--- crawleremporium/settings.py
+++ crawleremporium/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -64,7 +64,10 @@ ROOT_URLCONF = 'crawleremporium.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # include the templates directory
+            os.path.join(BASE_DIR, 'templates', 'allauth'),  # auth templates directory
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- crawleremporium/urls.py
+++ crawleremporium/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Added to include allauth URLs
     path('accounts/', include('allauth.urls')),
+    path('', include('lootboxes.urls')),
+
 ]

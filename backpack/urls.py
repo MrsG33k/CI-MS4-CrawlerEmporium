8014da116785a4d3889de51a5f707621f73bb7bd
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-02 22:30:13 +0100

    Edit: Added 'add to backpack' functionality

--- backpack/urls.py
+++ backpack/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_backpack, name='view_backpack')
+    path('', views.view_backpack, name='view_backpack'),
+    path('add/<item_id>/', views.add_to_backpack, name='add_to_backpack'),
 ]
--- backpack/views.py
+++ backpack/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_backpack(request):
     """ A view to return the backpack page"""
     return render(request, 'backpack/backpack.html')
+
+def add_to_backpack(request, item_id):
+    """Add an item to the backpack (cart)"""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    backpack = request.session.get('backpack',{})
+
+    if item_id in list(backpack.keys()):
+        backpack[item_id] += quantity
+    else:
+        backpack[item_id] = quantity
+    
+    request.session['backpack'] = backpack
+    print(request.session['backpack'])
+    return redirect(redirect_url)

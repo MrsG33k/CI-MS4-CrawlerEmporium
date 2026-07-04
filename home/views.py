652ce7ed3dfc77752afd507f873b924198252a43
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-04 22:09:45 +0100

    Add: Created carousel of Lootboxes on index.html using Flickity carousel.

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from lootboxes.models import Product
 
 def index(request):
-    """ A view to return the index page"""
-    return render(request, 'home/index.html')
+    """ A view to return the index page with dynamic loot boxes """
+    
+    # Fetch all the loot
+    products = Product.objects.all()
+    
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'home/index.html', context)

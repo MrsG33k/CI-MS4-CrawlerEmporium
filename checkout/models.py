99db6edd5e48053f4d775e98fe37c0f79318e654
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-04 11:40:18 +0100

    Add: Created the forms and admin for checkout

--- checkout/models.py
+++ checkout/models.py
@@ -5,8 +5,6 @@ from django.conf import settings
 
 from lootboxes.models import Product
 
-# Create your models here.
-
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -17,28 +15,27 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=60, null=False, blank=False)
     street_address1 = models.CharField(max_length=100, null=False, blank=False)
     street_address2 = models.CharField(max_length=100, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True,blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total=models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
         """Generate random order number using UUID"""
         return uuid.uuid4().hex.upper()
     
     def update_total(self):
-         """Update grand total each time a line item is added"""
+        """Update grand total each time a line item is added"""
 
-         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total_sum']
-         self.save()
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        # delivery is free, so grand total mirrors order total
+        self.grand_total = self.order_total
+        self.save()
 
-    
     def save(self, *args, **kwargs):
         """Override original save method to set the order number if it doesn't exist"""
-
         if not self.order_number:
             self.order_number = self._generate_order_number()
-        
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -46,14 +43,14 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineites')
+
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)    
     
     def save(self, *args, **kwargs):
-        """Override original save method to set the order number if it doesn't exist"""
-
+        """Override original save method to calculate line item pricing data records"""
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 

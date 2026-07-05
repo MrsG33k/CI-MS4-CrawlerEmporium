9031d3b14bbb562c6800363e468a5c9fb9ce27bc
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-05 15:42:45 +0100

    Add: Created the models.py for the showrunners blog section.

--- showrunners/admin.py
+++ showrunners/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import BlogPost, BlogComment
 
 # Register your models here.
+
+
+class BlogCommentInline(admin.TabularInline):
+    """ Allows admins to view and moderate comments directly inside the blog """
+    model = BlogComment
+    extra = 0
+    readonly_fields = ('user', 'message', 'created_at')
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_at')
+    search_fields = ('title', 'content')
+    # auto-populates the blog URL
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_at', 'author')
+    inlines = [BlogCommentInline]

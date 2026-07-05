60c8db942937dd9df93e325a94feb24b6a21d4ad
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-05 16:50:29 +0100

    Add: Created the blog.html and created 3 sample blog posts to check the layout.

--- showrunners/admin.py
+++ showrunners/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import BlogPost, BlogComment
 
-# Register your models here.
-
 
 class BlogCommentInline(admin.TabularInline):
     """ Allows admins to view and moderate comments directly inside the blog """

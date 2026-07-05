69fe379612de308feb02f3930f5d1314aaa3d118
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-05 15:47:26 +0100

    Add: Created forms.py and ran migrations for the showrunners blog and blog comments.

---
+++ showrunners/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import BlogComment
+
+class BlogCommentForm(forms.ModelForm):
+    class Meta:
+        model = BlogComment
+        fields = ['message']
+        labels = {
+            'message': 'Comment Input'
+        }
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['message'].widget.attrs.update({
+            'class': 'form-control bg-dark text-white border-secondary',
+            'placeholder': 'Enter commentary response packet...',
+            'rows': 3
+        })

976ae34db4b52ec3f0ae9b3825da692cd7bc2c01
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-05 11:40:57 +0100

    Add: Created the models and admin for the support ticket functionality.

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import SupportTicket
 
 # Register your models here.
+
+@admin.register(SupportTicket)
+class SupportTicktAdmin(admin.ModelAdmin):
+    list_display = (
+        'ticket_id_string',
+        'user',
+        'full_name',
+        'email',
+        'severity',
+        'is_resolved',
+        'created_at'
+    )
+
+    list_filter = ('is_resolved', 'severity', 'created_at')
+
+    search_fields = ('full_name', 'email', 'subject', 'message')
+
+    ordering = ['-id']
+
+    readonly_fields = ['created_at']
+
+    #Appends Tick# onto the front of the Ticket ID
+    @admin.display(description="TICK#", ordering="id")
+    def display_tick_id(self, obj):
+        return obj.id
--- support/models.py
+++ support/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class SupportTicket(models.Model):
+    # Severity status choices matching system lore
+    SEVERITY_CHOICES = [
+        ('low', 'Low - Minor Inconvenience'),
+        ('medium', 'Medium - Some feathers are fuffled here.'),
+        ('high', 'High - This is seriously a problem'),
+        ('critical', ' Critical - Its the end of the world as we know it'),
+    ]
+
+    # Links to the authenticated User if they are logged in. 
+    # Set blank=True and null=True so guests can complete the form
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='support_tickets')
+    
+    # Core contact information
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    
+    # Ticket details
+    subject = models.CharField(max_length=200)
+    severity = models.CharField(max_length=20, choices=SEVERITY_CHOICES, default='low')
+    message = models.TextField()
+
+    # Store a custom ticket string
+    ticket_id_string = models.CharField(max_length=20, unique=True, editable=False, blank=True, null=True)
+    
+    # Status
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_resolved = models.BooleanField(default=False)
+    resolution = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+    def save(self, *args, **kwargs):
+        """Add TICK to ID generation number after generation"""
+        super().save(*args, **kwargs)
+
+        if not self.ticket_id_string:
+            self.ticket_id_string = f"TICK{self.id}"
+            SupportTicket.objects.filter(id=self.id).update(ticket_id_string = self.ticket_id_string)
+
+
+    def __str__(self):
+        return self.ticket_id_string if self.ticket_id_string else "PENDING"

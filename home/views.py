3cfab9ab8d426041c46fe1b050c4827a2ff4d440
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-09 19:18:31 +0100

    Fix: Removed redundant white spaces and unused imports in home app

--- home/views.py
+++ home/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from lootboxes.models import Product
 
+
 def index(request):
     """ A view to return the index page with dynamic loot boxes """
-    
+
     # Fetch all the loot
     products = Product.objects.all()
-    
+
     context = {
         'products': products,
     }
-    
+
     return render(request, 'home/index.html', context)
